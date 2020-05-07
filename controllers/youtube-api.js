0e04640387cea17bd29a874630175bd777814499
Wouter Voogt <woutervoogt@gmail.com>
2020-05-07 15:13:09 +0200

    show api data on expertpage

--- controllers/youtube-api.js
+++ controllers/youtube-api.js
@@ -1,26 +1,28 @@
 // ... Youtube API ...
-const {google} = require("googleapis");
+const { google } = require("googleapis");
 
 // initialize the Youtube API library
 const youtube = google.youtube({
   version: "v3",
-  auth: process.env.APIKEY
+  auth: process.env.APIKEY,
 });
 
 // Get search list from youtube
 async function youtubeAPI() {
   const res = await youtube.search.list({
-      part: 'id,snippet',
-      maxResults: 10,
-      order: "viewCount",
-      q: 'Corona',
-      type: "video",
-    });
-    return res.data;
-  }
- 
-  if (module === require.main) {
-    youtubeAPI().catch(console.error);
-  }
+    part: "id,snippet",
+    maxResults: 10,
+    type: "video",
+    regionCode: "US",
+    order: "viewCount",
+    q: "Covid 19",
+    // videoCategoryId: "News & Politics",
+  });
+  return res.data;
+}
+
+if (module === require.main) {
+  youtubeAPI().catch(console.error);
+}
 
 module.exports = youtubeAPI;
